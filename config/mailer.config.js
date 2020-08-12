84598ef74d3d7bfdec4d6d986092f0bdfd030de3
Juan Romero <jrg89@hotmail.es>
2020-08-12 19:46:13 +0200

    mailer

--- config/mailer.config.js
+++ config/mailer.config.js
@@ -1,29 +1,28 @@
-require('dotenv').config();
-const nodemailer = require('nodemailer');
-const { getMaxListeners } = require('process');
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+const { getMaxListeners } = require('process')
 
-const host = process.env.HOST || 'http://localhost:3000';
-const user = process.env.NM_USER;
 
-const transport = nodemailer.createTransport(
-	{
-		service: 'Gmail',
-		auth: {
-			user: user,
-			pass: process.env.NM_PASS
-		}
-	}
-)
+const host = process.env.HOST || 'http://localhost:3000'
+const user = process.env.NM_USER
 
+
+const transport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: user,
+    pass: process.env.NM_PASS
+  }
+})
 module.exports.sendValidationEmail = (email, activationToken, name) => {
-	transport.sendMail({
-		to: email,
-		from: `Tweet hack team <${user}>`,
-		subject: 'Activate your account here!',
-		html: `
+  transport.sendMail({
+    to: email,
+    from: `Tweet hack team <${user}>`,
+    subject: 'Activate your account here!',
+    html: `
 			<h1>Hi ${name}</h1>
 			<p>Click on the button below to activate your account ❤️</p>
-			<a href="${host}/activate/${activationToken}" style="padding: 10px 20px; color: white; background-color: pink; border-radius: 5px;">Click here</a>
+			<a href="${host}/activate/${activationToken}" style="padding: 10px 20px; color: white; background-color: pink; border-radius: 5px;">${host}/activate/${activationToken}</a>
 		`
-	})
+  })
 }
